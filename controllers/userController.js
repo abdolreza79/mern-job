edf982aa1dcbcfe327eba5452d8d677c0a629c82
splinter <abdolreza79@gmail.com>
2024-03-24 14:40:29 +0330

    fixed update userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,6 +3,7 @@ import User from '../models/userModel.js'
 import Job from '../models/JobModel.js'
 import cloudinary from 'cloudinary'
 import { promises as fs } from 'fs'
+import { formatImage } from '../middleware/multerMiddleware.js'
 
 export const getCurrentUser = async (req, res, next) => {
   const user = await User.findById(req.user.userId)
@@ -10,11 +11,13 @@ export const getCurrentUser = async (req, res, next) => {
   res.status(StatusCodes.OK).json({ user: userWithoutPassword })
 }
 export const updateUser = async (req, res, next) => {
+  const file = formatImage(req.file)
   const newUser = { ...req.body }
   delete newUser.password
   if (req.file) {
-    const response = await cloudinary.v2.uploader.upload(req.file.path)
-    await fs.unlink(req.file.path)
+    const response = await cloudinary.v2.uploader.upload(file)
+    // const response = await cloudinary.v2.uploader.upload(req.file.path)
+    // await fs.unlink(req.file.path)
     newUser.avatar = response.secure_url
     newUser.avatarPublicId = response.public_id
   }
